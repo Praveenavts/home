12f6fd16e96639d13b8573640afcb7e9873f5e7d
Praveenavts <praveenavts@gmail.com>
2026-04-22 13:37:50 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import HomeContent
 
-
 @admin.register(HomeContent)
 class HomeContentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_active', 'created_at')
-    list_filter = ('is_active', 'created_at')
-    search_fields = ('title', 'description')
+    list_display = ('id', 'title', 'is_active')
